3f5ed554bfd52b385434b35201c1a5f238d7780e
William Fröhlich <41973611+wrfrohlich@users.noreply.github.com>
2020-06-22 10:09:47 -0300

    Add files via upload

---
+++ Docker/processing/processing.py
@@ -0,0 +1,68 @@
+#----------------------------------------------------------------------------------------------------------
+#
+#			Name: Eng. William da Rosa Frohlich
+#
+#			Project: Processing of BITalino data
+#
+#			Date: 2020.06.13
+#
+#----------------------------------------------------------------------------------------------------------
+#----------			Libraries			----------
+
+import json
+import socket
+import psycopg2
+
+#----------------------------------------------------------------------------------------------------------
+#----------			Variables			----------
+
+#----------			Variables - Socket			----------
+# Create the UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+address =('',18002)
+sock.bind(address)
+print ('Created Socket')
+
+#----------------------------------------------------------------------------------------------------------
+#----------			Functions			----------
+
+#----------			Function - Register in database - Raw data			----------
+def register_database (name, ecg, eda, time):
+	DSN = 'dbname=bitalino user=postgres host=database'		
+	connecting = psycopg2.connect(DSN)
+	SQL = 'INSERT INTO patients (name, ecg, eda, time) VALUES (%s, %s, %s, %s)'
+	cursorsql = connecting.cursor()
+	cursorsql.execute(SQL, (name, ecg, eda, time))
+	connecting.commit()
+	print ('Stored Data')
+	cursorsql.close()
+
+#----------------------------------------------------------------------------------------------------------
+#----------			Main Function			----------
+while True:
+	try:
+		packet, addr = sock.recvfrom(1024)
+		packet = (str(packet))
+		size = len(packet)
+		control = int(packet[2:3])
+		data = packet[3:(size-1)]
+			
+		if (control == 1):
+			name = data
+			print ('Received - Name')
+		elif (control == 2):
+			ECG = int(data)
+			print ('Received - ECG')
+		elif (control == 3):
+			EDA = int(data)
+			print ('Received - EDA')
+		elif (control == 4):
+			time = data
+			print ('Received - Time')
+		else:
+			register_database(name, ECG, EDA, time)
+	
+	except KeyboardInterrupt:
+		break
+#----------------------------------------------------------------------------------------------------------
